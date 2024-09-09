d3dbcd182b5d6f37f4899705e26c7a6b30d3a65f
RamiroPadro <ramirop@shifta.la>
2024-09-09 18:24:34 -0300

    Presentacion

---
+++ pages/inicio.tsx
@@ -0,0 +1,20 @@
+const Inicio = () => {
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-4xl font-bold text-center mt-10">
+                Desarrollo de frontend skills - SHIFTA
+            </h1>
+            <p className="text-center mt-10">
+                En esta página se presentan una serie de problemas y soluciones
+                solicitadas para el desarrollo de frontend skills en SHIFTA.
+            </p>
+            <p className="text-center mt-10">
+                Se presentan problemas y soluciones relacionadas con BEM y SASS,
+                Redux, Accesibilidad, CSS y otros conceptos, tecnologías y
+                técnicas.
+            </p>
+        </div>
+    );
+};
+
+export default Inicio;
